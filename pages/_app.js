6c924f77386925b1f53c597dd4556e721c620066
jiwon <jidole041214@naver.com>
2021-02-01 22:07:06 +0900

    초기 세팅

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,12 @@
+import Main from '../src/compnents/main'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <>
+  <Component {...pageProps} />
+  </>
+  )
 }
 
 export default MyApp
